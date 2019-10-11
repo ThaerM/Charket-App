aac88c8a8b2429c635f7d5c241d3dafa495eb582
Thaer AbuHureira <thaer.mosa13@hotmail.com>
2019-10-11 13:32:48 +0300

    First Commit for desgin app

---
+++ CharketApp/CharketApp/Pages/Signup/SupermarketRegistration.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CharketApp.Pages.Signup
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SupermarketRegistration : ContentPage
+    {
+        public SupermarketRegistration()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CharketApp/CharketApp/Pages/StartPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CharketApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : ContentPage
+    {
+        //List for save images from Android and IOS Files 
+        ObservableCollection<FileImageSource> imageSources = new ObservableCollection<FileImageSource>();
+
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //Sign the name of image
+            imageSources.Add("Carsoul1");
+            //Sign the name of image
+            imageSources.Add("Carsoul2");
+            //Sign the name of image
+            imageSources.Add("Carsoul3");
+            //Sign the list on Carousel image for view 
+            carouselImages.ItemsSource = imageSources;
+            //Start from image number 0 on the list
+            carouselImages.Position = 0;
+        }
+
+        private void ContinueHandler(object sender, EventArgs e)
+        {
+            //Check if the postion of list now is 2 as max or not
+            if (carouselImages.Position == 2)
+            {
+
+                //Show the loading bar
+                activityIndicator.IsVisible = true;
+                activityIndicator.IsRunning = true;
+                //Go to Next page "Login Page" 
+                App.Current.MainPage = new NavigationPage(new LoginPage());
+                //Hide the loading
+                activityIndicator.IsVisible = false;
+                activityIndicator.IsRunning = false;
+            }
+            else
+            {
+                //Go to next image from list 
+                carouselImages.Position++;
+            }
+        }
+    }
+}
